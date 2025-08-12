641be6474ed18518d67ec434fcdca3aef3cc1d84
anet-eny <anet-eny@gmail.com>
2025-08-12 16:13:40 +0200

    Create tailwind.config.js to set custom spacings and apply paddings to Header

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+  theme: {
+    extend: {
+      spacing: {
+        2500: "var(--spacing-2500)",
+        1600: "var(--spacing-1600)",
+        1000: "var(--spacing-1000)",
+        800: "var(--spacing-800)",
+        700: "var(--spacing-700)",
+        600: "var(--spacing-600)",
+        500: "var(--spacing-500)",
+        400: "var(--spacing-400)",
+        300: "var(--spacing-300)",
+        250: "var(--spacing-250)",
+        200: "var(--spacing-200)",
+        100: "var(--spacing-100)",
+      },
+    },
+  },
+};
