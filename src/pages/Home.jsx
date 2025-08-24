eb970bcbb4009c99cd087daea3a4fe5501649c52
anet-eny <anet-eny@gmail.com>
2025-08-24 11:32:19 +0200

    Add min-h-screen property to fix desktop bg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen">
       <Hero />
       <Main />
       <Footer />
