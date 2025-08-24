cd577c6f9b36f42fbaea14436d19133f2e27875d
anet-eny <anet-eny@gmail.com>
2025-08-24 20:38:55 +0200

    Add max-w to tablet and desktop versions of p in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ export default function Hero() {
       <img
         src={bgDesktop}
         alt=""
-        className="absolute -top-20 hidden md:block max-w-[1500px]"
+        className="absolute -top-20 hidden md:block max-w-[1600px]"
       />
       <img
         src={bgPattern}
@@ -29,10 +29,10 @@ export default function Hero() {
       />
       <div className="relative z-10 px-250 sm:px-500 md:px-1000">
         <Header />
-        <h1 className="mt-800 text-preset-3_5-bold sm:text-preset-2 md:text-preset-1 text-slate sm:max-w-lg md:max-w-4xl ">
+        <h1 className="mt-800 text-preset-3_5-bold sm:text-preset-2 md:text-preset-1 text-slate sm:max-w-lg md:max-w-4xl">
           We make your music sound extraordinary
         </h1>
-        <p className="mt-500 text-preset-5 sm:text-preset-4 sm:text-lg md:text-preset-4 text-slate">
+        <p className="mt-500 text-preset-5 sm:text-preset-4 sm:text-lg md:text-preset-4 text-slate sm:max-w-lg md:max-w-4xl">
           A system audio equalizer specifically designed for Android and iOS.
           Freely tune the way your music sounds with a professional grade
           parametric EQ &amp; volume mixer. Control bass, mids, treble, gain
